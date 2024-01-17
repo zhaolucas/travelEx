37699c1816876d5e493069329490e34671153ab7
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-01-17 18:31:44 +0000

    Added Javascript Pseudo Code

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// ---------------Pseudo Code--------------------
+// when User searches for a city (clicks search button - click event):
+// - Fetch data from a Travel API
+// - store the user input in a variable
+// - use a fetch api to get the hotel/transport data for that city
+// - store that city into local storage
+// ---------------------------------------------------------
+// when User searches for a country (clicks search button - click event):
+// - Fetch data from a currency API
+// - store the user input in a variable
+// - use a fetch api to get the currency data for that country
+// - store that country into local storage
+// ----------------------------------------------------------
+// - use data in local.storage to create a city button under the travel search area for city history
+// - use data in local.storage to create a country button under the currency search area for country history
+// - when you click the city button it re-displays the hotel/transport data for that city
+// - when you click the country button it re-displays the currency data for that country
